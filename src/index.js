b80fbace960202bb74e5bf4b6438db0ab3785783
KristinaGus <vitaliklavrenko@Vitaliks-MacBook-Pro.local>
2022-12-08 10:57:15 +0200

    clear

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// redux
-import { Provider } from 'react-redux'
-import { persistStore } from 'redux-persist'
-// store
-import store from './services/store'
 // components
 import App from './components/App'
 
 // persist the store
-// persistStore(store, null, () => {
-  ReactDOM.render(
-    (
-      <App />
-    ),
-    document.getElementById('root')
-  )
-// })
+ReactDOM.render(
+  (
+    <App />
+  ),
+  document.getElementById('root')
+)
